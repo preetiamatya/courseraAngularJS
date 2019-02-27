70f5a149c585416302a2ae166a59d7e8d51b40f6
preetiamatya <preetiamatya@gmail.com>
2019-02-27 20:42:51 +0100

    Assignment 1 solution

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 
 
 
@@ -18,7 +20,9 @@ import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    
   ],
   imports: [
     BrowserModule,
